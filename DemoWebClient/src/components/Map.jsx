94740b5c740b278e339f48fd5714d64d2eda6554
max1219 <bescenniy.2003@mail.ru>
2025-02-17 15:54:07 +0600

    class Component -> function

--- DemoWebClient/src/components/Map.jsx
+++ DemoWebClient/src/components/Map.jsx
@@ -1,53 +1,35 @@
-import { MapContainer, Polygon, TileLayer, GeoJSON } from "react-leaflet";
+import { MapContainer, GeoJSON } from "react-leaflet";
+import { useMapEvents } from 'react-leaflet/hooks'
 import 'leaflet/dist/leaflet.css';
 import "./Map.css";
-import { Component } from "react";
+import { useState } from 'react'
 
-export default class Map extends Component {
+export default function Map() {
 
-    constructor(props) {
-        super(props);
+    const [current, setCurrent] = useState({
+        "type": "Polygon",
+        "coordinates": [[[2, 0], [2, 1], [3, 1], [3, 0], [2, 0]]]
+    });
+    let [key, setKey] = useState(0);
 
-        this.current = {
-            "type": "Polygon",
-            "coordinates": [[[2, 0], [2, 1], [3, 1], [3, 0], [2, 0]]]
-        }
+    return (
+        <div>
+            <button onClick={() => {
+                for (let i = 0; i < current.coordinates[0].length; i++) {
+                    current.coordinates[0][i][0] += 1;
+                }
 
-        this.state = {
-            geojson: this.current
-        }
+                key++;
+                if (key > 100) {
+                    key = 0;
+                }
+                setKey(key);
 
+            }}>Click</button>
+            <MapContainer center={[0, 0]} zoom={5} scrollWheelZoom={true}>
+                <GeoJSON key={key} data={current} />
+            </MapContainer>
+        </div>
+    )
 
-        this.i = 0;
-
-        this.click = this.click.bind(this);
-    }
-
-    componentDidMount() {
-
-    }
-
-    click() {
-        for (let i = 0; i < this.current.coordinates[0].length; i++) {
-            this.current.coordinates[0][i][0] += 1;
-        }
-
-        this.setState({ geojson: this.current });
-    }
-
-
-    render() {
-        this.i++;
-        if (this.i > 100) {
-            this.i = 0;
-        }
-        return (
-            <div>
-                <button onClick={this.click}>Change</button>
-                <MapContainer center={[0, 0]} zoom={5} scrollWheelZoom={true}>
-                    <GeoJSON key={this.i} data={this.state.geojson} />
-                </MapContainer>
-            </div>
-        )
-    }
 }
--- DemoWebClient/src/pages/MainPage.jsx
+++ DemoWebClient/src/pages/MainPage.jsx
@@ -3,19 +3,6 @@ import "./MainPage.css"
 import Map from "../components/Map";
 
 function MainPage() {
-
-    let map = <Map />;
-    let geojson1 = {
-        "type": "Polygon",
-        "coordinates": [[[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]]]
-    }
-    let geojson2 = {
-        "type": "Polygon",
-        "coordinates": [[[2, 0], [2, 1], [3, 1], [3, 0], [2, 0]]]
-    }
-
-    //map.setData(geojson1);
-
     return (
     <div>
         <Map/>
