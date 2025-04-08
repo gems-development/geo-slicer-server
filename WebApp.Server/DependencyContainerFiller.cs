3faa981eb0b9bed31ca916c2d029148ccf8678ff
Daniil_Teslov <dteslov@gmail.com>
2025-04-08 11:11:45 +0600

    Moved filling DI container from DependencyContainerFiller to suitable extension methods in services and usecases

--- WebApp.Server/DependencyContainerFiller.cs
+++ WebApp.Server/DependencyContainerFiller.cs
@@ -1,11 +1,7 @@
 using UseCases;
-using UseCases.Interfaces;
-using DataAccess.Interfaces;
 using DataAccess.PostgreSql;
-using NetTopologySuite.Geometries;
 using Mediatr.Handlers;
 using Services.GeometryProviders;
-using Services.GeometryProviders.Interfaces;
 
 namespace WebApp.Server;
 
@@ -13,12 +9,12 @@ public static class DependencyContainerFiller
 {
     public static void Fill(ref WebApplicationBuilder builder, IConfigurationRoot configuration)
     {
-        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(GetByAreaQueryHandler)));
         var connectionString = configuration.GetConnectionString("DefaultConnection");
-        builder.Services.AddScoped<GeometryDbContext>(_ => new PostgreApplicationContext(connectionString!));
-        builder.Services.AddScoped<IGeometryInfoService<string>, GeometryInfoService>();
-        builder.Services.AddScoped<IGeometryByScreenService<Geometry>, GeometryByScreenService>();
-        builder.Services.AddScoped<IGeometryInfoProvider<string>, GeometryInfoProvider<string>>();
-        builder.Services.AddScoped<IGeometriesProvider<Geometry>, GeometriesProvider<Geometry>>();
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(GetByAreaQueryHandler)));
+        builder.Services.AddGeometryDbContext(connectionString!);
+        builder.Services.AddGeometryInfoService();
+        builder.Services.AddGeometryByScreenService();
+        builder.Services.AddGeometryInfoProvider();
+        builder.Services.AddGeometriesProvider();
     }
 }
