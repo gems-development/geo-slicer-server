8394258691efaea4aa96b6ebfbe1514510dcf297
Daniil_Teslov <dteslov@gmail.com>
2025-04-17 15:14:02 +0600

    Renamed TInfo to TId

--- WebApp.Dto.Responses/ClickInfoDto.cs
+++ WebApp.Dto.Responses/ClickInfoDto.cs
@@ -1,12 +1,12 @@
 namespace WebApp.Dto.Responses;
 
-public class ClickInfoDto<TInfo>
+public class ClickInfoDto<TId>
 {
     public string LayerAlias { get; set; }
     public string Properties { get; set; }
-    public TInfo Id { get; set; }
+    public TId Id { get; set; }
 
-    public ClickInfoDto(string layerAlias, string properties, TInfo id)
+    public ClickInfoDto(string layerAlias, string properties, TId id)
     {
         LayerAlias = layerAlias;
         Properties = properties;
